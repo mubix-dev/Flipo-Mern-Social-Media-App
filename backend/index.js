4c4a7c506f658fa20cc9855c2ae6d135429d7e06
Mubeen Khan <mubix.dev@gmail.com>
2026-05-08 23:37:05 +0500

    add postRouter

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
+import postRouter from "./routes/post.routes.js";
 
 dotenv.config({ override: true });
 
@@ -20,6 +21,7 @@ app.use(cors({
 
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
+app.use("/api/post",postRouter)
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
