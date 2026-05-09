6b5c9750ca779ae9eed3cdba3d0c0b7d6289c2fe
Mubeen Khan <mubix.dev@gmail.com>
2026-05-10 01:03:18 +0500

    add post story flip reducers

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit"
 import userSlice from "../redux/userSlice.js"
+import postSlice from "../redux/postSlice.js"
+import storySlice from "../redux/storySlice.js"
+import flipSlice from "../redux/flipSlice.js"
 const store = configureStore({
     reducer:{
-        user:userSlice
+        user:userSlice,
+        post:postSlice,
+        story:storySlice,
+        flip:flipSlice
     }
 })
 
