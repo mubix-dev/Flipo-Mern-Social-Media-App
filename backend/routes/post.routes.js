550bfdac33649242e16757b7972307ec37843f11
Mubeen Khan <mubix.dev@gmail.com>
2026-05-09 15:22:53 +0500

    update post routes

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,11 @@
 import {Router} from "express"
 import isAuth from "../middlewares/isAuth.js"
-import { commentPost, getAllMyPosts, likePost, savedPosts, uploadPost } from "../controllers/post.controller.js";
+import { commentPost, getAllPosts, likePost, savedPosts, uploadPost } from "../controllers/post.controller.js";
 import {upload} from "../config/multer.js"
 const postRouter = Router();
 
 postRouter.post("/upload",isAuth,upload.single("media"),uploadPost);
-postRouter.get("/getAllMyPosts",isAuth,getAllMyPosts);
+postRouter.get("/getAllPosts",isAuth,getAllPosts);
 postRouter.post("/like/:postId",isAuth,likePost);
 postRouter.get("/saved/:postId",isAuth,savedPosts);
 postRouter.post("/comment/:postId",isAuth,commentPost);
