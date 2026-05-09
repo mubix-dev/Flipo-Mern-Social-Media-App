c9e3fd854e38b0ccb77ee33d5d36efeb82d42782
Mubeen Khan <mubix.dev@gmail.com>
2026-05-09 15:22:42 +0500

    create flip routes

---
+++ backend/routes/flip.routes.js
@@ -0,0 +1,13 @@
+import {Router} from "express"
+import isAuth from "../middlewares/isAuth.js"
+import { commentFlip, getAllFlips, likeFlip, uploadFlip } from "../controllers/flip.controller.js";
+import {upload} from "../config/multer.js"
+const flipRouter = Router();
+
+flipRouter.post("/upload",isAuth,upload.single("video"),uploadFlip);
+flipRouter.get("/getAllFlips",isAuth,getAllFlips);
+flipRouter.post("/like/:flipId",isAuth,likeFlip);
+flipRouter.post("/comment/:flipId",isAuth,commentFlip);
+
+
+export default flipRouter;
