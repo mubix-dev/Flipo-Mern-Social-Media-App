bc4a81e710f413c2345dbeef86234e13d87710a4
Mubeen Khan <mubix.dev@gmail.com>
2026-05-16 20:55:02 +0500

    feat: add messageSlice to Redux store for messaging state management

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,12 +3,14 @@ import userSlice from "../redux/userSlice.js"
 import postSlice from "../redux/postSlice.js"
 import storySlice from "../redux/storySlice.js"
 import flipSlice from "../redux/flipSlice.js"
+import messageSlice from "../redux/messageSlice.js"
 const store = configureStore({
     reducer:{
         user:userSlice,
         post:postSlice,
         story:storySlice,
-        flip:flipSlice
+        flip:flipSlice,
+        message:messageSlice
     }
 })
 
