ae17aae601ccba8296408a868f290a50f24ece50
Mubeen Khan <mubix.dev@gmail.com>
2026-05-16 14:42:57 +0500

    fix: refactor getSuggestedUsers hook for improved readability and error handling

--- frontend/src/hooks/getSuggestedUsers.js
+++ frontend/src/hooks/getSuggestedUsers.js
@@ -1,26 +1,29 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { serverURL } from "../main"
-import { useDispatch, useSelector } from "react-redux"
-import { setSuggestedUsersData, setUserData } from "../redux/userSlice"
+import axios from "axios";
+import { useEffect } from "react";
+import { serverURL } from "../main";
+import { useDispatch, useSelector } from "react-redux";
+import { setSuggestedUsersData } from "../redux/userSlice";
 
+const getSuggestedUsers = () => {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
 
-const getSuggestedUsers = ()=>{
-    const dispatch = useDispatch();
-    const {userData} = useSelector(state=>state.user);
-    useEffect(()=>{
-        const fetchSuggetsedUsesData = async ()=>{
-            try {
-                const result = await axios.get(`${serverURL}/api/user/suggested`,{withCredentials:true});
-                dispatch(setSuggestedUsersData(result.data))
-            } catch (error) {
-                console.log(error)
-            }
-        }
+  useEffect(() => {
+    if (!userData) return;
 
-        fetchSuggetsedUsesData();
-    },[dispatch])
+    const fetchSuggestedUserData = async () => {
+      try {
+        const result = await axios.get(`${serverURL}/api/user/suggested`, {
+          withCredentials: true,
+        });
+        dispatch(setSuggestedUsersData(result.data));
+      } catch (error) {
+        console.log("Error loading suggested profiles:", error);
+      }
+    };
 
-}
+    fetchSuggestedUserData();
+  }, [dispatch, userData]); 
+};
 
 export default getSuggestedUsers;
