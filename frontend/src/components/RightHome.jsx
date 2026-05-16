0ad7d8f405579484f1e720f98bacca0e681d3891
Mubeen Khan <mubix.dev@gmail.com>
2026-05-16 20:54:20 +0500

    feat: add Messages component to RightHome for chat display

--- frontend/src/components/RightHome.jsx
+++ frontend/src/components/RightHome.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Messages from '../pages/ChatPage'
 
 function RightHome() {
   return (
     <div className='w-[25%] hidden min-h-screen lg:block bg-black border-l-2 border-gray-900'>
-      
+      <Messages/>
     </div>
   )
 }
