7c47df8c29649994c8792f95b34f065e4b8b0352
Mubeen Khan <mubix.dev@gmail.com>
2026-05-16 14:41:02 +0500

    fix: refactor getAllFlips hook to improve error handling and ensure userData is checked before fetching

--- frontend/src/hooks/getALLFlips.js
+++ frontend/src/hooks/getALLFlips.js
@@ -1,26 +1,29 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { serverURL } from "../main"
-import { useDispatch, useSelector } from "react-redux"
-import { setFlipData } from "../redux/flipSlice"
+import axios from "axios";
+import { useEffect } from "react";
+import { serverURL } from "../main";
+import { useDispatch, useSelector } from "react-redux";
+import { setFlipData } from "../redux/flipSlice";
 
+const getAllFlips = () => {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
 
-const getAllFlips = ()=>{
-    const dispatch = useDispatch();
-    const {flipData} = useSelector(state=>state.flip)
-    useEffect(()=>{
-        const fetchAllFlipsData = async ()=>{
-            try {
-                const result = await axios.get(`${serverURL}/api/flip/getAllFlips`,{withCredentials:true});
-                dispatch(setFlipData(result.data))
-            } catch (error) {
-                console.log(error)
-            }
-        }
+  useEffect(() => {
+    if (!userData) return;
 
-        fetchAllFlipsData();
-    },[dispatch])
+    const fetchAllFlipsData = async () => {
+      try {
+        const result = await axios.get(`${serverURL}/api/flip/getAllFlips`, {
+          withCredentials: true,
+        });
+        dispatch(setFlipData(result.data));
+      } catch (error) {
+        console.log("Error loading flips feed:", error);
+      }
+    };
 
-}
+    fetchAllFlipsData();
+  }, [dispatch, userData]);
+};
 
 export default getAllFlips;
