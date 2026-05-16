3466bbaa98342471a572b6b46868ab29d290bc49
Mubeen Khan <mubix.dev@gmail.com>
2026-05-16 14:32:12 +0500

    fix: update story routes to include getAllStories and correct route definitions

--- backend/routes/story.routes.js
+++ backend/routes/story.routes.js
@@ -1,13 +1,14 @@
 
 import {Router} from "express"
 import isAuth from "../middlewares/isAuth.js"
-import { getStoryByUsername, upladStory, viewStory } from "../controllers/story.controller.js";
+import { getAllStories, getStoryByUsername, upladStory, viewStory } from "../controllers/story.controller.js";
 import {upload} from "../config/multer.js"
 const storyRouter = Router();
 
 storyRouter.post("/upload",isAuth,upload.single("media"),upladStory);
-storyRouter.post("/view/:storyId",isAuth,viewStory);
-storyRouter.get("/story/:username",isAuth,getStoryByUsername);
+storyRouter.get("/view/:storyId",isAuth,viewStory);
+storyRouter.get("/storyByUsername/:username",isAuth,getStoryByUsername);
+storyRouter.get("/getAllStories",isAuth,getAllStories)
 
 
 export default storyRouter;
