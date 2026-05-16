827939be3725dcffbbe6a22e0a9c851fcf86681f
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 01:49:55 +0500

    feat: update store configuration to include socketSlice and improve middleware settings

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,18 +1,26 @@
-import {configureStore} from "@reduxjs/toolkit"
-import userSlice from "../redux/userSlice.js"
-import postSlice from "../redux/postSlice.js"
-import storySlice from "../redux/storySlice.js"
-import flipSlice from "../redux/flipSlice.js"
-import messageSlice from "../redux/messageSlice.js"
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "../redux/userSlice.js";
+import postSlice from "../redux/postSlice.js";
+import storySlice from "../redux/storySlice.js";
+import flipSlice from "../redux/flipSlice.js";
+import messageSlice from "../redux/messageSlice.js";
+import socketSlice from "../redux/socketSlice.js";
 const store = configureStore({
-    reducer:{
-        user:userSlice,
-        post:postSlice,
-        story:storySlice,
-        flip:flipSlice,
-        message:messageSlice
-    }
-})
-
+  reducer: {
+    user: userSlice,
+    post: postSlice,
+    story: storySlice,
+    flip: flipSlice,
+    message: messageSlice,
+    socket: socketSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["socket/setSocket"],
+        ignoredPaths: ["socket.socket"],
+      },
+    }),
+});
 
 export default store;
