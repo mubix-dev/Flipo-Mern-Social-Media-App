e2f191950c35f192944ca30dee39142f3d52adea
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 01:49:25 +0500

    feat: update ReceiverMsg component to display online user status with dynamic border color

--- frontend/src/components/ReceiverMsg.jsx
+++ frontend/src/components/ReceiverMsg.jsx
@@ -4,16 +4,17 @@ import { useSelector } from "react-redux";
 
 function ReceiverMsg({ msg }) {
   const { selectedUser } = useSelector((state) => state.message);
+  const { onlineUsers } = useSelector((state) => state.socket);
   const formatTime = (dateString) => {
     if (!dateString) return "";
     const date = new Date(dateString);
     return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
   };
-
+  const isOnline = onlineUsers.includes(selectedUser?._id)
   return (
     <div className="w-full flex items-start gap-2.5 px-2 animate-in fade-in slide-in-from-bottom-1 duration-200">
       
-      <div className="w-8 h-8 rounded-full overflow-hidden shrink-0 border border-zinc-800 mt-1 shadow-sm">
+      <div className={`w-8 h-8 rounded-full overflow-hidden shrink-0 border-2 ${isOnline?"border-green-500":"border-zinc-800"} mt-1 shadow-sm`}>
         <img
           className="w-full h-full object-cover aspect-square"
           src={selectedUser?.avatar || dp}
