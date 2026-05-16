d7362056a974d2cdbbb70b07417a53810c7320bf
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 01:48:47 +0500

    feat: add getFollowing route to user router

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from"express"
-import {editProfile, follow, getCurrentUser, getProfile, getSuggestedUsers } from "../controllers/user.controller.js"
+import {editProfile, follow, getCurrentUser, getFollowingList, getProfile, getSuggestedUsers } from "../controllers/user.controller.js"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../config/multer.js"
 const userRouter = Router()
@@ -9,4 +9,5 @@ userRouter.get("/suggested",isAuth,getSuggestedUsers)
 userRouter.post("/editProfile",isAuth,upload.single("avatar"),editProfile)
 userRouter.get("/getProfile/:username",isAuth,getProfile)
 userRouter.get("/follow/:targetedUserId",isAuth,follow)
+userRouter.get("/getFollowing",isAuth,getFollowingList)
 export default userRouter;
