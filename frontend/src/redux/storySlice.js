89b61d7a4520df19c00e73a1afca899a712df26e
Mubeen Khan <mubix.dev@gmail.com>
2026-05-16 14:47:41 +0500

    fix: update initial state structure in storySlice for storyData and allStoriesData

--- frontend/src/redux/storySlice.js
+++ frontend/src/redux/storySlice.js
@@ -3,14 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const storySlice = createSlice({
   name: "story",
   initialState: {
-    storyData: [],
+    storyData: null,
+    allStoriesData:[]
   },
   reducers: {
     setStoryData: (state, action) => {
       state.storyData = action.payload;
     },
+    setAllStoriesData: (state, action) => {
+      state.allStoriesData = action.payload;
+    },
   },
 });
 
-export const { setStoryData } = storySlice.actions;
+export const { setStoryData,setAllStoriesData } = storySlice.actions;
 export default storySlice.reducer;
