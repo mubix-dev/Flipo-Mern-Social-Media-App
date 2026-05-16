aa88446ab9673fa6aad39aef1f64b8d1ec9f6d87
Mubeen Khan <mubix.dev@gmail.com>
2026-05-16 14:35:54 +0500

    fix: add navigation to login on logout and improve suggested users rendering

--- frontend/src/components/LeftHome.jsx
+++ frontend/src/components/LeftHome.jsx
@@ -7,16 +7,22 @@ import axios from "axios";
 import { serverURL } from "../main";
 import { setUserData } from "../redux/userSlice";
 import OtherUser from "./OtherUser";
+import { useNavigate } from "react-router-dom";
 function LeftHome() {
   const { userData,suggestedUsers } = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleLogout = async()=>{
     try {
         await axios.get(`${serverURL}/api/auth/logout`,{withCredentials:true})
         dispatch(setUserData(null));
+        navigate("/login")
     } catch (error) {
         console.log(error);
     }
+  }
+  if(!userData){
+    return null
   }
   return (
     <div className="w-[25%] hidden min-h-screen lg:block bg-black border-r-2 border-gray-900">
@@ -44,11 +50,10 @@ function LeftHome() {
         </div>
         
       </div>
-        {/* {Suggested Users} */}
       <div className="w-full flex flex-col gap-5 my-5">
         <p className="text-white text-[16px] px-5 font-semibold">Suggested Users</p>
           {suggestedUsers && suggestedUsers.slice(0,3).map((user,index)=>(
-            <OtherUser key={index} user={user}/>
+            <OtherUser key={user._id} user={user}/>
           ))}
       </div>
       <div className="text-red-500 text-sm  fixed   bottom-5 left-62 cursor-pointer" onClick={handleLogout}>Log Out</div>
