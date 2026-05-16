71b524c64186a6bffbf3454adf7013c2164e04a4
Mubeen Khan <mubix.dev@gmail.com>
2026-05-16 14:41:26 +0500

    fix: refactor getAllPosts hook for improved userData handling and error logging

--- frontend/src/hooks/getAllPosts.js
+++ frontend/src/hooks/getAllPosts.js
@@ -1,27 +1,29 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { serverURL } from "../main"
-import { useDispatch, useSelector } from "react-redux"
-import { setPostData } from "../redux/postSlice"
+import axios from "axios";
+import { useEffect } from "react";
+import { serverURL } from "../main";
+import { useDispatch, useSelector } from "react-redux";
+import { setPostData } from "../redux/postSlice";
 
+const getAllPosts = () => {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
 
-const getAllPosts = ()=>{
-    const dispatch = useDispatch();
-    const {postData} = useSelector(state=>state.post)
-    const {userData} = useSelector(state=>state.user)
-    useEffect(()=>{
-        const fetchAllPostsData = async ()=>{
-            try {
-                const result = await axios.get(`${serverURL}/api/post/getAllPosts`,{withCredentials:true});
-                dispatch(setPostData(result.data))
-            } catch (error) {
-                console.log(error)
-            }
-        }
+  useEffect(() => {
+    if (!userData) return;
 
-        fetchAllPostsData();
-    },[dispatch])
+    const fetchAllPostsData = async () => {
+      try {
+        const result = await axios.get(`${serverURL}/api/post/getAllPosts`, {
+          withCredentials: true,
+        });
+        dispatch(setPostData(result.data));
+      } catch (error) {
+        console.log("Error loading posts feed:", error);
+      }
+    };
 
-}
+    fetchAllPostsData();
+  }, [dispatch, userData]);
+};
 
 export default getAllPosts;
