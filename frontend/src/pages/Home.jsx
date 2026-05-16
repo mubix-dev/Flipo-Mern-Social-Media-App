dfa26d13d790de9d256dbe21c6ecc709ee57f549
Mubeen Khan <mubix.dev@gmail.com>
2026-05-16 14:43:36 +0500

    fix: refactor Home component for improved loading state handling and code organization

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import LeftHome from '../components/LeftHome'
-import Feed from '../components/Feed'
-import RightHome from '../components/RightHome'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { ClipLoader } from 'react-spinners';
+import LeftHome from '../components/LeftHome';
+import Feed from '../components/Feed';
+import RightHome from '../components/RightHome';
+
 function Home() {
+  const { userData, suggestedUsers } = useSelector((state) => state.user);
+  const { postData } = useSelector((state) => state.post);
+  const { allStoriesData } = useSelector((state) => state.story);
+
+  if (!userData || !suggestedUsers || !postData || !allStoriesData) {
+    return (
+      <div className="h-screen w-full bg-black flex flex-col items-center justify-center text-white">
+        <ClipLoader color="#870ccf" size={50} />
+      </div>
+    );
+  }
+
   return (
-    <div className='w-full min-h-screen flex justify-center items-start '>     
-      <LeftHome/>
-      <Feed/>
-      <RightHome/>
+    <div className='w-full h-screen flex justify-center items-start overflow-hidden bg-black'>     
+      <LeftHome />
+      <Feed />
+      <RightHome />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
