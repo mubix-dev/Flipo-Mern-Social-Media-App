f549eee88836c46ed29fcb09bdf1128201be5c46
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 16:47:56 +0500

    feat: enhance ChatPage layout and integrate previous chat users display

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -3,26 +3,35 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import OnlineUsers from "../components/OnlineUsers";
+import PrevChatUsers from "../components/PrevChatUsers";
+
 
 function ChatPage() {
   const navigate = useNavigate();
   const { userData, followingList } = useSelector((state) => state.user);
   const { onlineUsers } = useSelector((state) => state.socket);
+  const { prevChatUsers } = useSelector((state) => state.message);
   return (
-    <div className="w-full min-h-screen flex flex-col gap-5 ">
+    <div className="w-full min-h-screen flex flex-col ">
       <div
-        className="text-[20px]  cursor-pointer text-white  flex items-center gap-2.5 lg:gap-5  py-6 px-5"
+        className="text-[20px]  cursor-pointer text-white  flex items-center gap-2.5 lg:gap-5  mt-5 px-5 "
         onClick={() => navigate("/")}
       >
         <FaArrowLeftLong className="lg:hidden" />
-        <div className="text-[15px] lg:text-[20px]">Chats</div>
+        <div className="text-[18px] lg:text-[22px] font-semibold">{onlineUsers?.length>1?"Active Friends":"Recent Chats"}</div>
       </div>
 
-      <div className="w-full min-h-screen flex flex-col overflow-y-scroll">
-  {followingList?.map((user) => (
-    <OnlineUsers key={user._id} user={user} />
-  ))}
-</div>
+      <div className="w-full  flex  gap-2.5 overflow-x-scroll py-2.5 border-b-2 border-gray-900 `">
+        {followingList?.map((user) => (
+          onlineUsers?.includes(user._id) && <OnlineUsers key={user._id} user={user} />
+        ))}
+      </div>
+      <div className="text-[14px] lg:text-[18px] mt-5 text-slate-400 px-5 font-semibold">{onlineUsers?.length>1?"Recent Chats":""}</div>
+      <div className="w-full  flex flex-col py-2.5  gap-2.5 overflow-y-scroll">
+        {prevChatUsers?.map((user) => (
+          <PrevChatUsers key={user._id} user={user} />
+        ))}
+      </div>
     </div>
   );
 }
