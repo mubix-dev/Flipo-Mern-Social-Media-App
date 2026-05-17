56b9c7446df066722c2c934e545aaf3b4e0f67a7
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 20:18:04 +0500

    feat: update user routes to include notification retrieval and mark as read functionality

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from"express"
-import {editProfile, follow, getCurrentUser, getFollowingList, getProfile, getSuggestedUsers, serach } from "../controllers/user.controller.js"
+import {editProfile, follow, getAllNotifications, getCurrentUser, getFollowingList, getProfile, getSuggestedUsers, markAsRead, search } from "../controllers/user.controller.js"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../config/multer.js"
 const userRouter = Router()
@@ -10,5 +10,7 @@ userRouter.post("/editProfile",isAuth,upload.single("avatar"),editProfile)
 userRouter.get("/getProfile/:username",isAuth,getProfile)
 userRouter.get("/follow/:targetedUserId",isAuth,follow)
 userRouter.get("/getFollowing",isAuth,getFollowingList)
-userRouter.get("/search",isAuth,serach)
+userRouter.get("/search",isAuth,search)
+userRouter.get("/notifications",isAuth,getAllNotifications)
+userRouter.post("/read",isAuth,markAsRead)
 export default userRouter;
