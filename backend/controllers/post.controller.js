ceed36d93611cdc438fe6abb3a9cfb53ab1942d2
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 20:17:33 +0500

    feat: add notification system for likes and comments on posts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,7 +1,8 @@
 import uploadOnCloudinary from "../config/cloudinary.js";
 import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
-import { io } from "../socket.js";
+import Notification from "../models/notification.model.js";
+import { getReceiverSocketId, io } from "../socket.js";
 const uploadPost = async (req, res) => {
   try {
     const { caption, mediaType } = req.body;
@@ -41,10 +42,10 @@ const uploadPost = async (req, res) => {
 
 const getAllPosts = async (req, res) => {
   try {
-    const posts = await Post.find({}).populate(
-      "author",
-      "name username avatar",
-    ).populate("comments.author","name username avatar").sort({createdAt:-1});
+    const posts = await Post.find({})
+      .populate("author", "name username avatar")
+      .populate("comments.author", "name username avatar")
+      .sort({ createdAt: -1 });
     return res.status(200).json(posts);
   } catch (error) {
     return res.status(500).json({ message: `getAllPosts error: ${error}` });
@@ -70,13 +71,31 @@ const likePost = async (req, res) => {
       );
     } else {
       post.likes.push(req.userId);
+      if (post.author._id != req.userId) {
+        const notification = await Notification.create({
+          sender: req.userId,
+          receiver: post.author._id,
+          type: "like",
+          message: "like your post",
+          post: post._id,
+        });
+
+        const populatedNotification = await Notification.findById(
+          notification._id,
+        ).populate("sender receiver post");
+
+        const receiverSocketId = getReceiverSocketId(post.author._id);
+        if (receiverSocketId) {
+          io.to(receiverSocketId).emit("newNotification", populatedNotification);
+        }
+      }
     }
     await post.save();
     await post.populate("author", "name username avatar");
-    io.emit("likePost",{
-      postId:post._id,
-      likes:post.likes
-    })
+    io.emit("likePost", {
+      postId: post._id,
+      likes: post.likes,
+    });
     return res.status(200).json(post);
   } catch (error) {
     return res.status(500).json({ message: `likePost error: ${error}` });
@@ -98,23 +117,42 @@ const commentPost = async (req, res) => {
       message,
     });
 
+    if (post.author._id != req.userId) {
+      const notification = await Notification.create({
+        sender: req.userId,
+        receiver: post.author._id,
+        type: "comment",
+        message: "comment on your post",
+        post: post._id,
+      });
+
+      const populatedNotification = await Notification.findById(
+        notification._id,
+      ).populate("sender receiver post");
+
+      const receiverSocketId = getReceiverSocketId(post.author._id);
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit("newNotification", populatedNotification);
+      }
+    }
+
     await post.save();
 
     const populatedPost = await Post.findById(postId)
       .populate("author", "name username avatar")
       .populate({
         path: "comments.author",
-        select: "name username avatar"
+        select: "name username avatar",
       });
 
-      io.emit("commentOnPost",{
-      postId:post._id,
-      comments:post.comments
-    })
+    io.emit("commentOnPost", {
+      postId: post._id,
+      comments: post.comments,
+    });
 
     return res.status(200).json(populatedPost);
   } catch (error) {
-    console.error(error); 
+    console.error(error);
     return res.status(500).json({ message: `Internal server error` });
   }
 };
@@ -148,5 +186,4 @@ const savedPosts = async (req, res) => {
   }
 };
 
-
-export { uploadPost, getAllPosts, likePost, commentPost,savedPosts };
+export { uploadPost, getAllPosts, likePost, commentPost, savedPosts };
