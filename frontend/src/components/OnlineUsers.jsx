baac4591cb7ecdeaf6db0c4d0b411c38992cdb64
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 16:47:02 +0500

    style: adjust padding and dimensions in OnlineUsers component

--- frontend/src/components/OnlineUsers.jsx
+++ frontend/src/components/OnlineUsers.jsx
@@ -14,10 +14,10 @@ function OnlineUsers({ user }) {
   const isOnline = onlineUsers?.includes(user?._id?.toString());
 
   return (
-    <div className="w-full flex justify-between items-center px-5 py-2">
+    <div className="w-full flex justify-between items-center px-5 py-2.5">
       <div className="flex justify-center items-center gap-2.5">
         
-        <div className="relative w-12 h-12">
+        <div className="relative w-10 h-10">
           
           <div
             className="w-full h-full rounded-full cursor-pointer overflow-hidden border border-zinc-800"
@@ -36,15 +36,6 @@ function OnlineUsers({ user }) {
           {isOnline && (
             <div className="w-3.5 h-3.5 absolute bg-green-500 rounded-full border-2 bottom-0 right-0 z-50 border-black"></div>
           )}
-        </div>
-
-        <div className="flex flex-col">
-          <div className="text-white text-[15px] font-semibold">
-            {user?.username}
-          </div>
-          <div className="text-gray-500 text-[12px]">
-            {isOnline ? "Active now" : user?.name || "Offline"}
-          </div>
         </div>
 
       </div>
