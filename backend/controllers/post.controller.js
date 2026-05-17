bb20829fb6e6821844a0fe08d726b86208c55d70
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 22:15:29 +0500

    feat: implement deletePost function to allow users to delete their own posts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -86,7 +86,10 @@ const likePost = async (req, res) => {
 
         const receiverSocketId = getReceiverSocketId(post.author._id);
         if (receiverSocketId) {
-          io.to(receiverSocketId).emit("newNotification", populatedNotification);
+          io.to(receiverSocketId).emit(
+            "newNotification",
+            populatedNotification,
+          );
         }
       }
     }
@@ -186,4 +189,35 @@ const savedPosts = async (req, res) => {
   }
 };
 
-export { uploadPost, getAllPosts, likePost, commentPost, savedPosts };
+const deletePost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const currUserId = req.userId;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    if (post.author.toString() !== currUserId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized! You can only delete your own posts" });
+    }
+
+    await Post.findByIdAndDelete(postId);
+
+    await User.findByIdAndUpdate(currUserId, {
+      $pull: { posts: postId },
+    });
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Post deleted successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `deletePost error: ${error.message}` });
+  }
+};
+
+export { uploadPost, getAllPosts, likePost, commentPost, savedPosts,deletePost };
