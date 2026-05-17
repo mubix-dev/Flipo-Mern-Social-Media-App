c233012b311e67054a49c0c6ab3e5afee952611a
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 16:46:21 +0500

    feat: add search route to user router for suggested users

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from"express"
-import {editProfile, follow, getCurrentUser, getFollowingList, getProfile, getSuggestedUsers } from "../controllers/user.controller.js"
+import {editProfile, follow, getCurrentUser, getFollowingList, getProfile, getSuggestedUsers, serach } from "../controllers/user.controller.js"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../config/multer.js"
 const userRouter = Router()
@@ -10,4 +10,5 @@ userRouter.post("/editProfile",isAuth,upload.single("avatar"),editProfile)
 userRouter.get("/getProfile/:username",isAuth,getProfile)
 userRouter.get("/follow/:targetedUserId",isAuth,follow)
 userRouter.get("/getFollowing",isAuth,getFollowingList)
+userRouter.get("/search",isAuth,serach)
 export default userRouter;
