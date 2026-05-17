91684048317d74017169c0fa484f2d9bce5a1e4e
Mubeen Khan <mubix.dev@gmail.com>
2026-05-17 22:15:55 +0500

    feat: add deletePost route to enable post deletion by users

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,6 +1,6 @@
 import {Router} from "express"
 import isAuth from "../middlewares/isAuth.js"
-import { commentPost, getAllPosts, likePost, savedPosts, uploadPost } from "../controllers/post.controller.js";
+import { commentPost, deletePost, getAllPosts, likePost, savedPosts, uploadPost } from "../controllers/post.controller.js";
 import {upload} from "../config/multer.js"
 const postRouter = Router();
 
@@ -9,6 +9,7 @@ postRouter.get("/getAllPosts",isAuth,getAllPosts);
 postRouter.post("/like/:postId",isAuth,likePost);
 postRouter.get("/saved/:postId",isAuth,savedPosts);
 postRouter.post("/comment/:postId",isAuth,commentPost);
+postRouter.delete("/delete/:postId",isAuth,deletePost);
 
 
 export default postRouter;
